ef49855a2ee382f69717e52e5aa9ab93ec5c2e32
ksa2112 <ksanetbrhane21@gmail.com>
2022-03-16 16:53:23 +0300

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
+#include "main.h"
+/**
+ * main -  Print the word "Holberton"
+ * Description: Write a program that prints _putchar
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
 
-char _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void pr_digit(int charcode, int fmt);
-int add(int a, int b);
-void pr_with_fmt(int charcode, int fmt);
-void print_to_98(int n);
-void pr_3_digit(int charcode, int fmt);
-void print_times_table(int n);
-void print_spaces(int n);
-void print_zero(void);
+int i;
+char j[] = "_putchar";
 
-#endif
+for (i = 0; i < 8; i++)
+
+{
+_putchar(j[i]);
+
+}
+_putchar('\n');
+
+return (0);
+}
